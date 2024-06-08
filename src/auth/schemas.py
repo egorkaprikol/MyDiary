6afc6a217134e09adbf3205628fd921ce0471676
Egor <zherelev92@mail.ru>
2024-06-09 00:56:54 +0300

    doing auth #2

---
+++ src/auth/schemas.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, ConfigDict
+from fastapi_users import schemas
+from fastapi_users.schemas import PYDANTIC_V2
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    login: str
+    nickname: str
+    created_at: Optional[datetime]
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    if PYDANTIC_V2:  # pragma: no cover
+        model_config = ConfigDict(from_attributes=True)  # type: ignore
+    else:  # pragma: no cover
+
+        class Config:
+            orm_mode = True
+
+
+class UserCreate(schemas.BaseUserCreate):
+    id: int
+    nickname: str
+    password: str
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
--- src/main.py
+++ src/main.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
+from src.auth.config import auth_backend
+from src.auth.database import User
+from src.auth.manager import get_user_manager
+from src.auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
-    title = "MyDiaryApp"
+    title="MyDiaryApp"
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
 )
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
